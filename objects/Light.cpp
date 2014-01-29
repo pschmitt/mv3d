25b49ce8583869d8446d7170e18fde9e49e19ea1
Philipp Schmitt <philipp@schmitt.co>
2014-01-29 01:32:09 +0100

    Add dummy light class

---
+++ objects/Light.cpp
@@ -0,0 +1,21 @@
+/*
+ * Light.cpp
+ *
+ *  Created on: Jan 29, 2014
+ *      Author: pschmitt
+ */
+
+#include "Light.h"
+
+namespace schmitt_co {
+
+Light::Light() {
+	// TODO Auto-generated constructor stub
+	
+}
+
+Light::~Light() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace schmitt_co */
---
+++ objects/Light.h
@@ -0,0 +1,21 @@
+/*
+ * Light.h
+ *
+ *  Created on: Jan 29, 2014
+ *      Author: pschmitt
+ */
+
+#ifndef LIGHT_H_
+#define LIGHT_H_
+
+namespace schmitt_co {
+
+class Light {
+	public:
+		Light();
+		virtual ~Light();
+};
+
+} /* namespace schmitt_co */
+
+#endif /* LIGHT_H_ */
