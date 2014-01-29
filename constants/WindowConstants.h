41dc823ea61e534fea88b7549abcbabd841b2c95
Philipp Schmitt <philipp@schmitt.co>
2014-01-29 01:07:53 +0100

    Update window size

--- constants/WindowConstants.h
+++ constants/WindowConstants.h
@@ -9,7 +9,7 @@
 #define WINDOWCONSTANTS_H_
 
 #define WINDOW_TITLE "MV3D - Projet - schmitt.co"
-#define WINDOW_HEIGHT 600
-#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 1024
+#define WINDOW_WIDTH 1280
 
 #endif /* WINDOWCONSTANTS_H_ */
